8d1efc642167ac23feea3dfe3c8e81e07722e228
druryyl <drury_yl@yahoo.com>
2023-03-07 22:44:52 +0700

    re-commit create login

--- btr.sql/Database/create-login.sql
+++ btr.sql/Database/create-login.sql
@@ -1,8 +1,15 @@
 ﻿CREATE LOGIN btrLogin
     WITH PASSWORD = 'btr123!';
+GO
 
 CREATE USER btrUser FOR LOGIN btrLogin;
+GO
 
-sp_addrolemember 'db_datareader', 'hlinikUser';
-sp_addrolemember 'db_datawriter','hlinikUser';
-sp_addrolemember 'db_ddladmin', 'hlinikUser';
+sp_addrolemember 'db_datareader', 'btrUser';
+GO
+
+sp_addrolemember 'db_datawriter','btrUser';
+GO
+
+sp_addrolemember 'db_ddladmin', 'btrUser';
+GO
