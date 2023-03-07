ae12e63fbf21b50e043cf87d137f2d8f53f72054
druryyl <drury_yl@yahoo.com>
2023-03-07 22:44:20 +0700

    scripting create database dan user

---
+++ btr.sql/Database/create-database.sql
@@ -0,0 +1,8 @@
+﻿CREATE DATABASE btr
+    ON (
+        NAME = 'btr_data',
+        FILENAME = 'd:\database\btr\btr_data.mdf')
+        LOG ON (
+        NAME = 'btr_log',
+        FILENAME = 'd:\database\btr\btr_log.ldf')
+GO
---
+++ btr.sql/Database/create-login.sql
@@ -0,0 +1,8 @@
+﻿CREATE LOGIN btrLogin
+    WITH PASSWORD = 'btr123!';
+
+CREATE USER btrUser FOR LOGIN btrLogin;
+
+sp_addrolemember 'db_datareader', 'hlinikUser';
+sp_addrolemember 'db_datawriter','hlinikUser';
+sp_addrolemember 'db_ddladmin', 'hlinikUser';
