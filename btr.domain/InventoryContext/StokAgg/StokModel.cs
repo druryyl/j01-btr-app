d9d1ca816c03f9928c65a750e7860abe891d9877
druryyl <drury_yl@yahoo.com>
2023-03-08 15:16:27 +0700

    Persiapan Project

--- btr.domain/InventoryContext/StokAgg/StokModel.cs
+++ btr.domain/InventoryContext/StokAgg/StokModel.cs
@@ -12,5 +12,4 @@ public class StokModel
     public string BrgSatuanId { get; set; }
     public string BrgId { get; set; }
     public string BrgName { get; set; }
-    public 
 }
--- btr.sql/Database/create-database.sql
+++ btr.sql/Database/create-database.sql
@@ -1,8 +1,8 @@
-﻿CREATE DATABASE btr
-    ON (
-        NAME = 'btr_data',
-        FILENAME = 'd:\database\btr\btr_data.mdf')
-        LOG ON (
-        NAME = 'btr_log',
-        FILENAME = 'd:\database\btr\btr_log.ldf')
-GO
+﻿--CREATE DATABASE btr
+--    ON (
+--        NAME = 'btr_data',
+--        FILENAME = 'd:\database\btr\btr_data.mdf')
+--        LOG ON (
+--        NAME = 'btr_log',
+--        FILENAME = 'd:\database\btr\btr_log.ldf')
+--GO
