ec993252609ccbfecb2fcdbc38c7c2eb87dd92f8
Jude <drury_yl@yahoo.com>
2023-06-28 12:59:46 +0700

    Dev use-case GetBrgQuery.cs

---
+++ btr.api/Controllers/InventoryContext/BrgController.cs
@@ -0,0 +1,27 @@
+﻿using btr.application.InventoryContext.BrgAgg.UseCases;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Nuna.Lib.ActionResultHelper;
+
+namespace btr.api.Controllers.InventoryContext;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BrgController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public BrgController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("{id}")]   
+    public async Task<IActionResult> CreateFaktur(string id)
+    {
+        var query = new GetBrgQuery(id);
+        var result = await _mediator.Send(query);
+        return Ok(new JSendOk(result));
+    }
+}
---
+++ btr.application/InventoryContext/BrgAgg/UseCases/GetBrgQuery.cs
@@ -0,0 +1,57 @@
+﻿using btr.application.InventoryContext.BrgAgg.Workers;
+using btr.domain.InventoryContext.BrgAgg;
+using Dawn;
+using Mapster;
+using MediatR;
+
+namespace btr.application.InventoryContext.BrgAgg.UseCases;
+
+public record GetBrgQuery(string BrgId) : IRequest<GetBrgResponse>, IBrgKey;
+
+public class GetBrgResponse
+{
+    public string BrgId { get; set; }
+    public string BrgName { get; set;}
+    public List<GetBrgResponseSatuanHrg> ListSatuanHarga { get; set; }
+}
+
+public class GetBrgResponseSatuanHrg
+{
+    public string BrgId { get; set; }
+    public string Satuan { get; set; }
+    public int Conversion { get; set; }
+    public int Stok { get; set; }
+    public double HargaJual { get; set; }
+}
+
+public class GetBrgHandler : IRequestHandler<GetBrgQuery, GetBrgResponse>
+{
+    private BrgModel _aggRoot = new();
+    private IBrgBuilder _builder;
+
+    public GetBrgHandler(IBrgBuilder builder)
+    {
+        _builder = builder;
+    }
+
+    public Task<GetBrgResponse> Handle(GetBrgQuery request, CancellationToken cancellationToken)
+    {
+        //  GUARD
+        Guard.Argument(() => request)
+            .Member(x => x.BrgId, y => y.NotEmpty());
+        
+        //  QUERY
+        _aggRoot = _builder
+            .Load(request)
+            .Build();
+        
+        //  RESPONSE
+        return Task.FromResult(GenResponse());
+    }
+
+    private GetBrgResponse GenResponse()
+    {
+        var result = _aggRoot.Adapt<GetBrgResponse>();
+        return result;
+    }
+}
